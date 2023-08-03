41eaa86bc76f8c60c3b9cfbd8764a8c970ad8c6a
Prathibha Ratnayake <prsthibha@gmail.com>
2023-08-03 16:19:40 +0530

    complete dashboard page

---
+++ actions/get-stock-count.ts
@@ -0,0 +1,11 @@
+import prisma from '@/lib/prisma';
+
+export const getStockCount = async (storeId: string) => {
+  const stockCount = await prisma.product.count({
+    where: {
+      storeId,
+      isArchived: false,
+    },
+  });
+  return stockCount;
+};
